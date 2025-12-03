f59c58c140c332a08657821df65b980c128cf16b
JSX4820 <danabogil@alum.us.es>
2025-12-03 17:25:21 +0100

    ejercicio  7

--- src/extranjeria.py
+++ src/extranjeria.py
@@ -1,5 +1,5 @@
 # - IMPORTS:
-from typing import NamedTuple, List
+from typing import NamedTuple, List, Tuple
 import csv
 RegistroExtranjeria = NamedTuple("RegistroExtranjeria", [("distrito",str), ("seccion", str), ("barrio", str), ("pais",str), ("hombres", int), ("mujeres", int)])
 
@@ -42,12 +42,52 @@ def total_extranjeros_por_pais(registros:List[RegistroExtranjeria]) -> dict[str,
     return dict_aux
 
 # - EJERCICIO 5:
-def top_n_extranjeria(registros:List[RegistroExtranjeria], n:int = 3) -> List[tuple]:
+def top_n_extranjeria(registros:List[RegistroExtranjeria], n:int = 3) -> List[Tuple[str, int]]:
     dict_aux = total_extranjeros_por_pais(registros)
     mas_poblacion = [(clave, valor) for clave, valor in dict_aux.items()]
     mas_poblacion.sort(key=lambda m:m[1], reverse=True)
     return mas_poblacion[:n]
 
 # - EJERCICIO 6:
+def total_paises_por_barrio(registros:List[RegistroExtranjeria]) -> dict[str, set[str]]:
+    dict_aux = dict()
+    for registro in registros:
+        if registro.barrio in dict_aux:
+            dict_aux[registro.barrio].add(registro.pais)
+        else:
+            dict_aux[registro.barrio] = set(registro.pais)
+    return dict_aux
+
 def barrios_mas_multicultural(registros:List[RegistroExtranjeria]) -> str:
-    
+    dict_aux = total_paises_por_barrio(registros)
+    list_aux = [(clave, len(valor)) for clave, valor in dict_aux.items()]
+    return max(list_aux, key=lambda x:x[1])
+
+# - EJERCICIO 7:
+def barrio_con_mas_extranjeros(registros:List[RegistroExtranjeria], tipo:str = None) -> str:
+    dict_aux = total_extranjeros_por_barrio(registros, tipo)
+    return max(dict_aux.keys(), key=dict_aux.get)
+
+def total_extranjeros_por_barrio(registros:List[RegistroExtranjeria], tipo:str = None) -> dict[str, int]:
+    dict_aux = dict()
+    if tipo == "Hombres":
+        for registro in registros:
+            if registro.pais in dict_aux:
+                dict_aux[registro.barrio] += registro.hombres
+            else:
+                dict_aux[registro.barrio] = registro.hombres
+        return dict_aux
+    elif tipo == "Mujeres":
+        for registro in registros:
+            if registro.pais in dict_aux:
+                dict_aux[registro.barrio] += registro.mujeres
+            else:
+                dict_aux[registro.barrio] = registro.mujeres
+        return dict_aux
+    else:
+        for registro in registros:
+            if registro.pais in dict_aux:
+                dict_aux[registro.barrio] += (registro.hombres + registro.mujeres)
+            else:
+                dict_aux[registro.barrio] = (registro.hombres + registro.mujeres)        
+        return dict_aux
--- src/extranjeria_test.py
+++ src/extranjeria_test.py
@@ -9,9 +9,15 @@ def test_total_extranjeros_por_pais(registros:List[RegistroExtranjeria]) -> None
     print(f"Estos son el total de extranjeros que hay por cada país: {total_extranjeros_por_pais(registros)}")
 def test_top_n_extranjeria(registros:List[RegistroExtranjeria], n:int = 3) -> None:
     print(f"Estos son los {n} países con más población extranjera: {top_n_extranjeria(registros, n)}")
+def test_barrio_mas_multicultural(registros:List[RegistroExtranjeria]) -> None:
+    print(f"Este es el barrio más multicultural: {barrios_mas_multicultural(registros)}")
+def test_barrio_con_mas_extranjeros(registros:List[RegistroExtranjeria], tipo:str = None) -> None:
+    print(f"Este es el barrio con más extranjeros: {barrio_con_mas_extranjeros(registros, tipo)}")
 if __name__ == "__main__":
     registros = lee_datos_extranjeria("data\extranjeriaSevilla.csv")
     #test_numero_nacionalidades_distintas(registros)
     #test_secciones_nacionalidades_distintas(registros, {"ITALIA", "ALEMANIA"})
     #test_total_extranjeros_por_pais(registros)
-    test_top_n_extranjeria(registros)
+    #test_top_n_extranjeria(registros)
+    #test_barrio_mas_multicultural(registros)
+    test_barrio_con_mas_extranjeros(registros, "Hombres")
